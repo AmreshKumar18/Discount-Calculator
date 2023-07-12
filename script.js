b4bf4a550ff8567b31e0dd8bbd21d577fe077d9f
Amresh Kumar <96064040+AmreshKumar18@users.noreply.github.com>
2023-07-12 16:38:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let days = document.getElementById("day")
+let hrs = document.getElementById("hrs")
+let mins = document.getElementById("mins")
+let sec = document.getElementById("sec")
+
+var dest = new Date ("july 8, 2023 01:42:30")
+
+var x = setInterval(function(){
+var now = new Date()
+var diff = (dest - now)
+var daycalc = Math.floor(diff/(1000*60*60*24))
+var hrscalc = Math.floor(diff%(1000*60*60*24)/(1000*60*60))
+var minscalc = Math.floor(diff%(1000*60*60)/(1000*60))
+var seccalc = Math.floor(diff%(1000*60)/1000)
+
+days.innerText = daycalc
+hrs.innerText = hrscalc
+mins.innerText = minscalc
+sec.innerText = seccalc
+
+if (diff == 0) {
+    clearInterval(countdownInterval);
+  }
+
+
+},1000);
